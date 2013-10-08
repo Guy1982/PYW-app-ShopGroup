cc5edfdbe09bf85e813796e976bce775f04e9a52
Guy Shachar <guys@sears.co.il>
2013-10-08 14:44:25 +0300

    remove space

--- app-ShopGroup-master/trunk/src/SywApplicationShopGroup.Web.UI/Controllers/LandingController.cs
+++ app-ShopGroup-master/trunk/src/SywApplicationShopGroup.Web.UI/Controllers/LandingController.cs
@@ -11,7 +11,6 @@ namespace SywApplicationShopGroup.Web.UI.Controllers
     public class LandingController : Controller
     {
         private readonly Routes _routes;
-		
         private readonly IAuthApi _authApi;
         private readonly IUsersApi _usersApi;
         private readonly IAppActionsApi _appActionsApi;
